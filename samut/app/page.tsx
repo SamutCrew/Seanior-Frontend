bd51b2cf2d1bab50a8e45cad7ad22e4fec45380b
Louisrelax <112146664+Louisrelax@users.noreply.github.com>
2025-02-10 14:08:44 +0700

    change type script

---
+++ samut/app/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Link from "next/link";
+
+
+  export default function Home() {
+    <div>
+      Hellooo
+      </div>
+
+  }
+
+
